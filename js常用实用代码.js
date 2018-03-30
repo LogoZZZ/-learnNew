095ab5f7f4cf289fabae6515dc6398561818f69e
zyj <1240850839@qq.com>
2018-03-30 21:30:32 +0800

    3.30

--- js常用实用代码.js
+++
@@ -1,6 +0,0 @@
-function classOf(o){
-    if(o===null)return "null";
-    if(o===undefined)return "undefined";
-    return Object.prototype.toString.call(o).slice(8,-1);
-}
-//classOf获取对象的类型，例如classOf([]) => Array
